45b9364fb9229cbacfa5e65dff6c45fd24e0695f
GabrielSerafim <gabriel.am.serafa@gmail.com>
2021-04-14 23:11:20 -0300

    iniciei a tela de buscar

--- app/src/main/java/com/example/exercicio2serafim/BuscarActivity.java
+++ app/src/main/java/com/example/exercicio2serafim/BuscarActivity.java
@@ -1,14 +1,33 @@
 package com.example.exercicio2serafim;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.exercicio2serafim.services.ProductService;
 
 public class BuscarActivity extends AppCompatActivity {
+
+    private ListView listaProdutos;
+    private Retrofit retrofit;
+    private ProductService pService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar);
+        inicializarComponentes();
+    }
+
+    private void inicializarComponentes() {
+        listaProdutos = findViewById(R.id.lista_produtos);
+        retrofit = new Retrofit.Builder()
+                .baseUrl(getResources().getString(R.string.IP))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        pService =retrofit.create(ProductService.class);
     }
 }
